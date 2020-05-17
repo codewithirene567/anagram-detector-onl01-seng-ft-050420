31c05218d8b441b9b30bc3bd0f808a5fb11e4e80
Irene Scott <irenejinscott@gmail.com>
2020-05-17 13:36:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+
+  def initialization
+    @word = word
+  end
+  
+  def match(array)
+    empty_array = []
+    word.each do |word|
+      if word.sort == array.sort
+      else empty_array
+  end
+  end
+  end
+  
+end
+    #takes an array of possible anagrams
+    #return all matches in an array
+    #If no matches exist, then it should return an empty array
+    
+#%w(foo bar) is a shortcut for ["foo", "bar"]. Meaning it's a notation to write an array of strings separated by spaces instead of commas and without quotes around them.
