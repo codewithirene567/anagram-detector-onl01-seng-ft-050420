c2014b2f675feb319442b5e9f41f330eaf359dda
Irene Scott <irenejinscott@gmail.com>
2020-05-17 13:59:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,26 +1,3 @@
-require "pry"
-
-class Anagram
-  attr_accessor :word
-
-  def initialization(word)
-    @word = word
-  end
-  
-  def match(array)
-    array.select do |element| #return the array of words that makes this true
-    (@word.split("").sort == element.split("").sort)
-    binding.pry
-  end
-  end
-
-  
-end
-    #takes an array of possible anagrams
-    #return all matches in an array
-    #If no matches exist, then it should return an empty array
-    
-#%w(foo bar) is a shortcut for ["foo", "bar"]. Meaning it's a notation to write an array of strings separated by spaces instead of commas and without quotes around them.
 
 class Anagram
   
