72205fbb92e8a7e4528d4f0a0046d3eb8ba0c0c4
Irene Scott <irenejinscott@gmail.com>
2020-05-17 13:38:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -22,3 +22,4 @@ end
     #If no matches exist, then it should return an empty array
     
 #%w(foo bar) is a shortcut for ["foo", "bar"]. Meaning it's a notation to write an array of strings separated by spaces instead of commas and without quotes around them.
+
